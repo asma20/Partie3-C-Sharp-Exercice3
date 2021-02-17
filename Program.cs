52b6a078c9f3777250add3af26a8f5b690a9cbb0
asma20 <jbeli.asma20@gmail.com>
2021-02-17 15:55:43 +0100

    Créer une variable result. Demander à l’utilisateur un nombre à additionner puis s’il veut ajouter un nombre. Tant que l’utilisateur ne répond pas non, redemander un nombre et l’ajouter au résultat.
    Afficher le résultat dans une phrase.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Partie3Exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result=0;
+            int nbr=0;
+            Boolean choix = false;
+
+
+            String rep;
+
+        
+
+          
+            while (!choix ) {
+                Console.WriteLine("Voulez vous ajouter un nombre o/n ");
+                rep = Console.ReadLine();
+                if (rep == "o")
+                {
+
+                    Console.WriteLine("Entrer un nombre ");
+                    nbr = Int32.Parse(Console.ReadLine());
+                    choix = false;
+                    result += nbr;
+                }
+                else
+                {
+                    choix = true;
+                }
+            }
+           
+            Console.WriteLine($"la somme des valeur ajoute est {result} ");
+
+
+
+        }
+    }
+}
